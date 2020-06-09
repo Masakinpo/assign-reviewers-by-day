f8c42f1e892928456332cfcc7ebf59284d05bc59
masakinpo <masakinpo.jp@gmail.com>
2020-06-09 13:10:44 +0900

    apply

--- src/config.ts
+++ src/config.ts
@@ -1,60 +1,75 @@
-import { getInput, setFailed } from "@actions/core";
-import { safeLoad } from 'js-yaml'
-import { readFileSync } from 'fs'
+import { getInput, setFailed } from '@actions/core';
+import { safeLoad } from 'js-yaml';
+import { readFileSync } from 'fs';
 
-export const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"] as const
+export const dayOfWeek = [
+  'mon',
+  'tue',
+  'wed',
+  'thu',
+  'fri',
+  'sat',
+  'sun',
+] as const;
 const listOfValidDay = [
   ...dayOfWeek,
-  "weekday",
-  "weekend",
-  "everyday"
+  'weekday',
+  'weekend',
+  'everyday',
 ] as const;
 
-const kindType = "must" as const;
+const kindType = 'must' as const;
 
 export type ReviewerType = {
-  name: string
-  kind?: typeof kindType
-  day: (typeof listOfValidDay)[number];
-}
+  name: string;
+  kind?: typeof kindType;
+  day: typeof listOfValidDay[number];
+};
 
 type NumOfReviewersType = {
-  must: number
-  other: number
-}
+  must: number;
+  other: number;
+};
 
 export type Config = {
-  reviewers: ReviewerType[]
-  numOfReviewers: NumOfReviewersType
-}
+  reviewers: ReviewerType[];
+  numOfReviewers: NumOfReviewersType;
+};
 
 export const getConfig = (): Config | null => {
-  const configPath = getInput('config', {required: true})
+  const configPath = getInput('config', { required: true });
 
   try {
-    return safeLoad(readFileSync(configPath, 'utf8'))
+    return safeLoad(readFileSync(configPath, 'utf8'));
   } catch (error) {
-    setFailed(error.message)
+    setFailed(error.message);
   }
 
-  return null
-}
+  return null;
+};
 
 export const validateConfig = (config: Config): boolean => {
   // validate day
-  if (config.reviewers.some(r => !!r.day && !listOfValidDay.includes(r.day))) {
+  if (
+    config.reviewers.some((r) => !!r.day && !listOfValidDay.includes(r.day))
+  ) {
     return false;
   }
 
   // validate number of reviewers
-  const { must: expectedNumMustReviewer, other: expectedNumOtherReviewer} = config.numOfReviewers;
-  const numMustReviewers = config.reviewers.filter(e => e.kind === "must").length;
+  const {
+    must: expectedNumMustReviewer,
+    other: expectedNumOtherReviewer,
+  } = config.numOfReviewers;
+  const numMustReviewers = config.reviewers.filter((e) => e.kind === 'must')
+    .length;
   const numOtherReviewers = config.reviewers.length - numMustReviewers;
 
-  if (config.reviewers.length < 1 ||
+  if (
+    config.reviewers.length < 1 ||
     numMustReviewers < expectedNumMustReviewer ||
     numOtherReviewers < expectedNumOtherReviewer
   )
     return false;
-  return true
-}
+  return true;
+};
--- src/handler.ts
+++ src/handler.ts
@@ -1,74 +1,124 @@
-import { Octokit } from "@octokit/rest";
+import { Octokit } from '@octokit/rest';
 import { addDays, format, isWeekend } from 'date-fns';
 import _ from 'lodash';
-import {Config, dayOfWeek, ReviewerType} from "./config";
+import { Config, dayOfWeek, ReviewerType } from './config';
 
-const mustReviewerCond = (r: ReviewerType): boolean => r.kind === "must";
-const otherReviewerCond = (r: ReviewerType): boolean => !r.kind || r.kind !== "must";
+const mustReviewerCond = (r: ReviewerType): boolean => r.kind === 'must';
+const otherReviewerCond = (r: ReviewerType): boolean =>
+  !r.kind || r.kind !== 'must';
 
 type ReviewerDict = {
   must: {
-    [key in (typeof dayOfWeek)[number] ]: ReviewerType[]
-  },
+    [key in typeof dayOfWeek[number]]: ReviewerType[];
+  };
   other: {
-    [key in (typeof dayOfWeek)[number] ]: ReviewerType[]
-  }
-}
+    [key in typeof dayOfWeek[number]]: ReviewerType[];
+  };
+};
 
 const generateDictFromConfig = (reviewers: ReviewerType[]): ReviewerDict => {
   const reviewerDict = {};
-  dayOfWeek.forEach(
-    d => {
-      const availableReviewers = reviewers.filter(r =>
+  dayOfWeek.forEach((d) => {
+    const availableReviewers = reviewers.filter(
+      (r) =>
         r.day === d ||
         !r.day ||
-        r.day === "everyday" ||
-        ((d === 'sat' || d === 'sun') && r.day === "weekend") ||
-        ((d !== 'sat' && d !== 'sun') && r.day === "weekday"))
-      _.set<ReviewerDict>(reviewerDict, ["must", d], availableReviewers.filter(r => mustReviewerCond(r)))
-      _.set<ReviewerDict>(reviewerDict, ["other", d], availableReviewers.filter(r => otherReviewerCond(r)))
-    }
-  )
+        r.day === 'everyday' ||
+        ((d === 'sat' || d === 'sun') && r.day === 'weekend') ||
+        (d !== 'sat' && d !== 'sun' && r.day === 'weekday')
+    );
+    _.set<ReviewerDict>(
+      reviewerDict,
+      ['must', d],
+      availableReviewers.filter((r) => mustReviewerCond(r))
+    );
+    _.set<ReviewerDict>(
+      reviewerDict,
+      ['other', d],
+      availableReviewers.filter((r) => otherReviewerCond(r))
+    );
+  });
   return reviewerDict as ReviewerDict;
-}
+};
 
-const setReviewers = async (octokit: Octokit, reviewers: string[]): Promise<object> => {
-  const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/')
-  const pr = Number((process.env.GITHUB_REF || '').split('refs/pull/')[1].split('/')[0])
+const setReviewers = async (
+  octokit: Octokit,
+  reviewers: string[]
+): Promise<object> => {
+  const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
+  const pr = Number(
+    (process.env.GITHUB_REF || '').split('refs/pull/')[1].split('/')[0]
+  );
 
   return octokit.pulls.createReviewRequest({
     owner,
     repo,
     reviewers,
     pull_number: pr, // eslint-disable-line @typescript-eslint/camelcase
-  })
+  });
 };
 
-export const assignReviewers = async (octokit: Octokit, config: Config): Promise<void> => {
-  const today =  new Date();
+export const assignReviewers = async (
+  octokit: Octokit,
+  config: Config
+): Promise<void> => {
+  const today = new Date();
   const reviewerDict = generateDictFromConfig(config.reviewers);
 
   // select must reviewers
-  const selectedMustReviewers: ReviewerType[] = reviewerDict.must[format(today, 'iii').toLowerCase() as typeof dayOfWeek[number]];
+  const selectedMustReviewers: ReviewerType[] =
+    reviewerDict.must[
+      format(today, 'iii').toLowerCase() as typeof dayOfWeek[number]
+    ];
   let count = 1;
   while (selectedMustReviewers.length < config.numOfReviewers.must) {
-    const nextDayMustReviewers = reviewerDict.must[format(addDays(today, count), 'iii').toLowerCase() as typeof dayOfWeek[number]];
-    selectedMustReviewers.length + nextDayMustReviewers.length < config.numOfReviewers.must ?
-      selectedMustReviewers.concat(nextDayMustReviewers) :
-      selectedMustReviewers.concat(_.sampleSize(nextDayMustReviewers, config.numOfReviewers.must - selectedMustReviewers.length));
+    const nextDayMustReviewers =
+      reviewerDict.must[
+        format(
+          addDays(today, count),
+          'iii'
+        ).toLowerCase() as typeof dayOfWeek[number]
+      ];
+    selectedMustReviewers.length + nextDayMustReviewers.length <
+    config.numOfReviewers.must
+      ? selectedMustReviewers.concat(nextDayMustReviewers)
+      : selectedMustReviewers.concat(
+          _.sampleSize(
+            nextDayMustReviewers,
+            config.numOfReviewers.must - selectedMustReviewers.length
+          )
+        );
     count++;
   }
 
   // select other reviewers
-  const selectedOtherReviewers: ReviewerType[] = reviewerDict.other[format(today, 'iii').toLowerCase() as typeof dayOfWeek[number]];
+  const selectedOtherReviewers: ReviewerType[] =
+    reviewerDict.other[
+      format(today, 'iii').toLowerCase() as typeof dayOfWeek[number]
+    ];
   count = 1;
   while (selectedOtherReviewers.length < config.numOfReviewers.other) {
-    const nextDayOtherReviewers = reviewerDict.other[format(addDays(today, count), 'iii').toLowerCase() as typeof dayOfWeek[number]];
-    selectedOtherReviewers.length + nextDayOtherReviewers.length < config.numOfReviewers.other ?
-      selectedOtherReviewers.concat(nextDayOtherReviewers) :
-      selectedOtherReviewers.concat(_.sampleSize(nextDayOtherReviewers, config.numOfReviewers.other - selectedOtherReviewers.length))
+    const nextDayOtherReviewers =
+      reviewerDict.other[
+        format(
+          addDays(today, count),
+          'iii'
+        ).toLowerCase() as typeof dayOfWeek[number]
+      ];
+    selectedOtherReviewers.length + nextDayOtherReviewers.length <
+    config.numOfReviewers.other
+      ? selectedOtherReviewers.concat(nextDayOtherReviewers)
+      : selectedOtherReviewers.concat(
+          _.sampleSize(
+            nextDayOtherReviewers,
+            config.numOfReviewers.other - selectedOtherReviewers.length
+          )
+        );
     count++;
   }
 
-  await setReviewers(octokit, [...selectedMustReviewers, ...selectedOtherReviewers].map(r => r.name))
-}
+  await setReviewers(
+    octokit,
+    [...selectedMustReviewers, ...selectedOtherReviewers].map((r) => r.name)
+  );
+};
