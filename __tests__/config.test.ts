4e7a4883a0ac40cb3a47d96239b4d92386e7519c
Shintaro Yamaguchi <programmer2844@gmail.com>
2020-06-10 00:17:00 +0900

    add validate config test but doesn't work...

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -1,6 +1,12 @@
-test('test', () => {
-  expect(3).toBe(3);
-});
+import { Config, validateConfig } from './../src/config';
 
 //TODO
 // add test for validateConfig
+
+describe('validate config test', () => {
+  const config: Config = {
+    reviewers: [{ name: 'messi', kind: 'must', day: ['mon'] }],
+    numOfReviewers: { must: 1, other: 1 },
+  };
+  expect(validateConfig(config)).toBe(true);
+})
